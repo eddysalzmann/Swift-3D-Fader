de89670d9fd8cedc2b4b6a33ce98d2a00d4bf396
Eddy Salzmann <hello@eddysalzmann.com>
2020-11-26 13:25:34 +0100

    Initial Commit

---
+++ Swift-3D-Fader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-3D-Fader
+//
+//  Created by Eddy Salzmann on 26.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift-3D-Fader/Swift_3D_FaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_3D_FaderApp.swift
+//  Swift-3D-Fader
+//
+//  Created by Eddy Salzmann on 26.11.20.
+//
+
+import SwiftUI
+
+@main
+struct Swift_3D_FaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
